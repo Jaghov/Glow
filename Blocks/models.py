feat,fix: added Preprocess layer to quantize and dequantize the input data, modified jacobian returned by affine and preprocessing to be batchwise, and changed loss function to accomodate discrete values

Blocks/models.py
@@ -3,6 +3,54 @@ import torch.nn as nn
 from torch.utils.data import DataLoader, sampler
 import torch.nn.functional as F
 
+class Preprocess(nn.Module):
+  def __init__(self, alpha=0.05):
+    super(Preprocess, self).__init__()
+    self.alpha = alpha
+
+
+  def forward(self, x):
+    '''
+    x (torch.tensor): shape input image [individual pixel values
+    should be in the range 0-255]
+
+    returns 
+    y (torch.tensor):
+    log_det_jacobian (torch.tensor): shape (B x 1)
+    '''
+
+    # Add jittering/noise ,u from uniform dist to model continous
+    # variable p(y) = p( x + u )
+
+    u = torch.rand_like(x)
+    eps = 1e-5 # To prevent the image from being geq 1
+    y = x *(255. -eps) + u
+
+
+    y = (self.alpha + (1 - self.alpha)*y/256.)
+
+  
+    # convert to logit
+    logit_y = y.log() - (1-y).log()
+
+
+    # compute the jacobian of this step
+    log_det_jacobian = ( ( (1- self.alpha) / y) + (1-self.alpha) / (1-y) ).view(x.shape[0], -1).sum(-1)
+
+
+    return logit_y, log_det_jacobian
+  
+  def inverse(self, logit_y):
+
+    x = logit_y.exp()/( 1+ logit_y.exp())
+
+
+
+    return x
+
+
+
+
 class ActNorm(nn.Module):
   def __init__(self, num_channels, height, width):
     super(ActNorm, self).__init__()
@@ -20,6 +68,8 @@ class ActNorm(nn.Module):
       self.bias.data = -torch.mean(x, (0,2,3), keepdim=True)
       self.log_scale.data = -torch.log(torch.std(x, (0,2,3), keepdim=True))
       self.initialised=True
+
+    # check dimensionality of jacobian
 
     return (x * torch.exp(self.log_scale)) + self.bias, self.h * self.w * self.log_scale.sum()
 
@@ -58,7 +108,7 @@ class Invertible1x1Conv2d(nn.Module):
 
     W = torch.matmul(self.P, torch.matmul(self.L*self.L_mask +self.identity, (self.U*self.U_mask + (torch.exp(self.log_s) * self.s_sign ).diag() ))).unsqueeze(2).unsqueeze(3)
 
-    log_det_W = torch.log(torch.exp(self.log_s)).sum() # Might need to learn log_scale instead?
+    log_det_W = torch.log(torch.exp(self.log_s)).sum()
     self.weights_updated = True
 
     return F.conv2d(x, W, bias=None), x.shape[2]* x.shape[3] * log_det_W
@@ -159,7 +209,7 @@ class AffineCoupling(nn.Module):
 
       y = torch.cat([y_a, y_b], dim=1)
 
-      return y, log_scale.sum().to(device=x.device)
+      return y, log_scale.view(x.shape[0], -1).sum(-1).to(device=x.device)
 
     def inverse(self, y):
       y_a, y_b = y.chunk(2, dim=1)
@@ -185,7 +235,7 @@ class GlowStep(nn.Module):
     ])
 
   def forward(self, x):
-    log_det_jacobian_total = torch.tensor(0.0).to(device=x.device)
+    log_det_jacobian_total = torch.zeros(x.shape[0], device=x.device)
     z = x
 
     for layer in self.layers:
@@ -205,7 +255,7 @@ class Glow(nn.Module):
   def __init__(self, n_flow_steps=3, n_flow_layers=3, n_channels=3, height=32, width=32):
     super(Glow, self).__init__()
 
-    # self.preprocess = Preprocess()
+    self.preprocess = Preprocess()
     self.flow_layers = nn.ModuleList()
 
 
@@ -257,8 +307,10 @@ class Glow(nn.Module):
 
   def forward(self, x):
 
-    log_det_jacobian_total = torch.tensor(0.0, device=x.device)
-    z_i = x
+    log_det_jacobian_total = torch.zeros(x.shape[0], device=x.device)
+    z_i, log_det_jacobian = self.preprocess(x)
+
+    log_det_jacobian_total += log_det_jacobian
 
 
     for layer in self.flow_layers:
@@ -290,7 +342,13 @@ class Glow(nn.Module):
         z_i = flow_step.inverse(z_i)
       z_i = self.unsqueeze2d(z_i)
 
-    x = z_i
+    x = self.preprocess.inverse(z_i)
 
 
     return x
+
+
+# x = torch.rand((3,3))
+# layer = Preprocess()
+
+# print(layer(x))

train_test.py
@@ -37,17 +37,14 @@ print(f'Using {device}')
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+     #transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+     ])
 
-#      # Inverse Transform (Unnormalize and Convert to PIL)
-# inverse_transform = transforms.Compose([
-#     transforms.Normalize((-1.0, -1.0, -1.0), (2.0, 2.0, 2.0)),  # Unnormalize
-#     transforms.ToPILImage()  # Convert back to PIL Image
-# ])
+
 
 
 # Necessary Hyperparameters
-num_epochs = 100
+num_epochs = 1000
 learning_rate = 5e-5
 batch_size = 128
 
@@ -76,8 +73,8 @@ classes = ('plane', 'car', 'bird', 'cat',
 dataiter = iter(trainloader)
 images, _ = next(dataiter)
 
-# print(images.shape)
-# print(images[0].min(), images[0].max())
+print(images.shape)
+print(images[0].min(), images[0].max())
 
 # # show images
 # show(make_grid(images))
@@ -108,7 +105,7 @@ def denorm(img):
 #   print(torch.allclose(batch, b))
 #   print(mse(batch, b))
 
-model = Glow(n_flow_steps=3, n_flow_layers=3).to(device)
+model = Glow(n_flow_steps=12, n_flow_layers=4).to(device)
 params = sum(p.numel() for p in model.parameters() if p.requires_grad)
 print("Total number of parameters is: {}".format(params))
 # print(model)
@@ -140,6 +137,8 @@ def grad_size(model):
 
 def evaluate(model, title):
     # *CODE FOR PART 1.2b IN THIS CELL*
+    normalised = False
+    
 
     # load the model
     print('Input images')
@@ -148,7 +147,9 @@ def evaluate(model, title):
     sample_inputs, _ = next(iter(testloader))
     fixed_input = sample_inputs[0:32, :, :, :]
     # visualize the original images of the last batch of the test set
-    img = make_grid(denorm(fixed_input), nrow=8, padding=2, normalize=False,
+    if normalised is True:
+        fixed_input = denorm(fixed_input)
+    img = make_grid(fixed_input, nrow=8, padding=2, normalize=False,
                     value_range=None, scale_each=False, pad_value=0)
 
     save_image(img, f'results/{title}_source_image.png')
@@ -171,7 +172,10 @@ def evaluate(model, title):
 
 
         recon_batch = recon_batch.cpu()
-        recon_batch = make_grid(denorm(recon_batch), nrow=8, padding=2, normalize=False,
+        if normalised is True:
+            recon_batch = denorm(recon_batch)
+
+        recon_batch = make_grid(recon_batch, nrow=8, padding=2, normalize=False,
                                 value_range=None, scale_each=False, pad_value=0)
 
         save_image(recon_batch, f'results/{title}_recon_image.png')
@@ -193,7 +197,9 @@ def evaluate(model, title):
         #######################################################################
 
         samples = samples.cpu()
-        samples = make_grid(denorm(samples), nrow=8, padding=2, normalize=False,
+        if normalised is True:
+            samples = denorm(samples)
+        samples = make_grid(samples, nrow=8, padding=2, normalize=False,
                                 value_range=None, scale_each=False, pad_value=0)
         save_image(samples, f'results/{title}_sampled_image.png')
         # plt.figure(figsize = (8,8))
@@ -231,25 +237,41 @@ def test_layers(x, model):
 
     return
 
-
+pi = torch.tensor(np.pi)
 
 torch.cuda.empty_cache()
-def loss_function_Glow(target_distribution, z, log_det_jacobian):
-  # print(log_det_jacobian)
-  log_likelihood = target_distribution.log_prob(z).mean() + log_det_jacobian/z.shape[0]
-  return -log_likelihood
+def loss_function_Glow(z, log_det_jacobian, n_bins=torch.tensor(256.)):
+  # fit z to normal distribution
+  log_p_z = -0.5 * (z ** 2 + torch.log(2 * pi ))
 
-from torch.distributions.normal import Normal
+  n_pixels = np.prod(z.size()[1:]) # H x W x C
+  c = n_pixels * torch.log(n_bins)
+
+
+
+
+  log_likelihood = log_p_z + log_det_jacobian.view(-1, 1, 1, 1) + c
+  loss = -log_likelihood
+
+  loss_in_nats = (loss / (torch.log(torch.tensor(2.))  * n_pixels)).mean()
+  log_p_z_in_nats = (log_p_z / (torch.log(torch.tensor(2.))  * n_pixels)).mean()
+  jacobian_in_nats = (log_det_jacobian / (torch.log(torch.tensor(2.))  * n_pixels)).mean()
+
+  return (
+    loss_in_nats,
+    log_p_z_in_nats, 
+    jacobian_in_nats,
+  )
+
+
 
 model.train()
-target_distribution = Normal(torch.tensor(0, dtype=torch.float32).to(device),torch.tensor(1, dtype=torch.float32).to(device))
-# <- You may wish to add logging info here
 n_samples = 6
 sample = torch.randn(n_samples, 3, 32, 32)
 for epoch in range(num_epochs):
     total_loss = 0 # <- You may wish to add logging info here
     err = 0
-    max_grad = 0
+    #max_grad = 0
 
     with tqdm.tqdm(trainloader, unit="batch") as tepoch:
         for batch_idx, (data, _) in enumerate(tepoch):
@@ -265,7 +287,7 @@ for epoch in range(num_epochs):
             z, log_det_jacobian = model.forward(data)
 
             # compute loss
-            loss = loss_function_Glow(target_distribution, z, log_det_jacobian)
+            loss, prior, jacobian = loss_function_Glow( z, log_det_jacobian)
 
 
             # backwards
@@ -273,15 +295,13 @@ for epoch in range(num_epochs):
             loss.backward()
 
             # Clip gradient
-            torch.nn.utils.clip_grad_norm_(model.parameters(), max_clip)
+            #torch.nn.utils.clip_grad_norm_(model.parameters(), max_clip)
 
             # update params
             optimizer.step()
 
             # Logging
             total_loss += loss.item()
-            prior = target_distribution.log_prob(z).mean()
-            jacobian = log_det_jacobian/z.shape[0]
             
 
 
@@ -296,10 +316,10 @@ for epoch in range(num_epochs):
                 with torch.no_grad():
                     err = mse(data, model.inverse(z)).mean()
                 tepoch.set_description(f"Epoch {epoch}")
-                avg_grad, max_grad = grad_size(model)
-                tepoch.set_postfix(loss=loss.item()/len(data), log_prior=prior.item(), log_jacobian=jacobian.item(), recon_err = err.item(), avg_weights=avg_grad.item(), max_grad=max_grad.item() )
+                #avg_grad, max_grad = grad_size(model)
+                tepoch.set_postfix(loss=loss.item()/len(data), log_prior=prior.item(), log_jacobian=jacobian.item(), recon_err = err.item() )#, avg_weights=avg_grad.item(), max_grad=max_grad.item() )
 
-    if epoch % 5 == 0:
+    if epoch % 10 == 0:
         rand_sample(model, epoch, sample)
 
     # save the model